7d5d9b58be535e6e1abc52168e0ba358192de457
流年释残忆 <291753906@qq.com>
2018-03-12 16:17:32 +0800

    Initial Commit

---
+++ PrivateTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PrivateTest
+//
+//  Created by iosyf-02 on 2018/3/12.
+//  Copyright © 2018年 情风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
